6e6729fa0fed589dc6497e9383d069c7b06f647e
laurgu <lgu011@uottawa.ca>
2023-07-14 20:34:38 -0400

    improved navigation

--- src/Templates/Home.jsx
+++ src/Templates/Home.jsx
@@ -1,13 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const formType = document.getElementById("formType").value;
+    if (formType === "intake") {
+      navigate("/intake");
+    } else if (formType === "order") {
+      navigate("/order");
+    }
+  };
   return (
-    <div>
+    <div style={{ margin: "2vw 2vw 2vw 2vw" }}>
       <h1>HOME</h1>
-      <Link to="/intake">Intake</Link>
-      <br></br>
-      <Link to="/order">Order</Link>
+      <hr></hr>
+      <div className="row">
+        <div className="col-6">
+          <h4>Create New Form</h4>
+
+          <form onSubmit={handleFormSubmit}>
+            <select id="formType">
+              <option value="" disabled selected hidden>
+                ---
+              </option>
+              <option value="intake">Intake</option>
+              <option value="order">Order</option>
+            </select>
+            <input
+              type="submit"
+              value="create"
+              style={{ marginLeft: "1vw" }}
+            ></input>
+          </form>
+        </div>
+        <div className="col-6">
+          <h4>Past Forms</h4>
+        </div>
+      </div>
     </div>
   );
 }
