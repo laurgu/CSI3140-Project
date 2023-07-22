ce12cc4ad1e1c53c0bc8df6e2619d5af948121b3
laurgu <lgu011@uottawa.ca>
2023-07-21 20:42:09 -0400

    eslint file + super small fixes

--- api/routes/documents.js
+++ api/routes/documents.js
@@ -3,88 +3,86 @@ const router = new express.Router();
 const Document = require("../models/document");
 
 router.get("/documents", async (req, res, next) => {
-    try {
-        const title = req.query.title || "";
-        const _id = req.query._id || "";
-        const client = req.query.client || "";
+  try {
+    const title = req.query.title || "";
+    const _id = req.query._id || "";
+    const client = req.query.client || "";
 
-        let documents;
-        if (_id !== "") {
-            console.log("HIT GET BY ID");
-            documents = await Document.findById(_id);
-        } else {
-            documents = await Document.find({
-                title: {$regex: "" + title, $options: "i"},
-                client: {$regex: "" + client, $options: "i"},
-            });
-        }
-
-        console.log(_id);
-        console.log("\n\nNew Request");
-        console.log(req.originalUrl);
-        console.log(req.query);
-        console.log(documents);
-
-        res.json(documents);
-    } catch (error) {
-        console.log("Errors in GET: ");
-        console.log(error);
-        next(error);
+    let documents;
+    if (_id !== "") {
+      console.log("HIT GET BY ID");
+      documents = await Document.findById(_id);
+    } else {
+      documents = await Document.find({
+        title: { $regex: "" + title, $options: "i" },
+        client: { $regex: "" + client, $options: "i" },
+      });
     }
+
+    console.log(_id);
+    console.log("\n\nNew Request");
+    console.log(req.originalUrl);
+    console.log(req.query);
+    console.log(documents);
+
+    res.json(documents);
+  } catch (error) {
+    console.log("Errors in GET: ");
+    console.log(error);
+    next(error);
+  }
 });
 
-router.put("/documents", async (req, res, next) => {
-    try {
-        console.log("HIT PUT");
-        console.log(req.body);
-        const filter = {_id: req.body._id};
-        const update = req.body;
-        const options = {
-            upsert: true,
-            new: true,
-        };
-        if (!req.body._id) {
-            console.log("Creating new document");
-            const temp = "Untitled" + Math.floor(Math.random() * 1000);
-            req.body.title = req.body.title || temp;
+router.put("/documents", async (req, res) => {
+  try {
+    console.log("HIT PUT");
+    console.log(req.body);
+    const filter = { _id: req.body._id };
+    const update = req.body;
+    const options = {
+      upsert: true,
+      new: true,
+    };
+    if (!req.body._id) {
+      console.log("Creating new document");
+      const temp = "Untitled" + Math.floor(Math.random() * 1000);
+      req.body.title = req.body.title || temp;
 
-            await Document.create(req.body);
-            const newdoc = await Document.findOne({title: req.body.title}).exec();
-            console.log("New document: ");
-            console.log(newdoc);
-            if (req.body.title === temp) {
-                const update = { title: newdoc._id };
-                const filter = { _id: newdoc._id };
-                await Document.findByIdAndUpdate(filter, update);
-            }
-            req.body._id = newdoc._id;
-        } else {
-            console.log("Updating document " + req.body._id)
-            await Document.findOneAndUpdate(filter, update, options);
-        }
-
-        res.send({
-            _id: req.body._id,
-        });
-
-    } catch (error) {
-        console.log("Errors in PUT: ");
-        console.log(error);
+      await Document.create(req.body);
+      const newdoc = await Document.findOne({ title: req.body.title }).exec();
+      console.log("New document: ");
+      console.log(newdoc);
+      if (req.body.title === temp) {
+        const update = { title: newdoc._id };
+        const filter = { _id: newdoc._id };
+        await Document.findByIdAndUpdate(filter, update);
+      }
+      req.body._id = newdoc._id;
+    } else {
+      console.log("Updating document " + req.body._id);
+      await Document.findOneAndUpdate(filter, update, options);
     }
+
+    res.send({
+      _id: req.body._id,
+    });
+  } catch (error) {
+    console.log("Errors in PUT: ");
+    console.log(error);
+  }
 });
 
 router.get("/recent", async (req, res, next) => {
-    try {
-        let documents = await Document.find({}).sort({_id: -1}).limit(5);
-        res.json(documents);
-        console.log("\n\nNew Request for Recent Documents");
-        console.log(documents);
-    } catch (error) {
-        console.log("Errors in GET: ");
-        console.log(error);
-        next(error);
-    }
+  try {
+    let documents = await Document.find({}).sort({ _id: -1 }).limit(5);
+    res.json(documents);
+    console.log("\n\nNew Request for Recent Documents");
+    console.log(documents);
+  } catch (error) {
+    console.log("Errors in GET: ");
+    console.log(error);
+    next(error);
+  }
 });
-
 
 module.exports = router;
--- web/src/Templates/Home.jsx
+++ web/src/Templates/Home.jsx
@@ -1,86 +1,69 @@
-import React, {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {fetchData, putData} from "../api/documents";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { fetchData, putData } from "../api/documents";
 import Search from "../Components/Search";
 
 function Home() {
-    const navigate = useNavigate();
-    const [documentList, setDocumentList] = useState([]);
+  const navigate = useNavigate();
+  const [documentList, setDocumentList] = useState([]);
 
-    useEffect(() => {
-        const fetchDocuments = async () => {
-            const query = {};
-            try {
-                const documents = await fetchData(query, 'recent');
-                setDocumentList(documents);
-            } catch (error) {
-                console.error("Error fetching documents:", error);
-            }
-        };
-
-        fetchDocuments();
-    }, []);
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        const formType = document.getElementById("formType").value;
-        if (formType === "intake") {
-            navigate("/intake");
-        } else if (formType === "order") {
-            navigate("/order");
-        } else if (formType === "custom") {
-            const id = (await putData({
-                    title: "New",
-                    client: "Untitled",
-                    type: "custom",
-                    author: "Untitled",
-                }))['_id']
-            navigate(`/document/${id}`);
-        }
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      const query = {};
+      try {
+        const documents = await fetchData(query, "recent");
+        setDocumentList(documents);
+      } catch (error) {
+        console.error("Error fetching documents:", error);
+      }
     };
 
-    return (
-        <div style={{margin: "2vw 2vw 2vw 2vw"}}>
-            <h1>HOME</h1>
-            <hr/>
-            <div className="row">
-                <div className="col-6">
-                    <h4>Create New Form</h4>
-                    <form onSubmit={handleFormSubmit}>
-                        <select id="formType">
-                            <option value="" disabled selected hidden>
-                                ---
-                            </option>
-                            <option value="intake">Intake</option>
-                            <option value="order">Order</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                        <input type="submit" value="create" style={{marginLeft: "1vw"}}/>
-                    </form>
-                    <Search/>
-                </div>
-                <div className="col-6">
-                    <h4>Recent Forms</h4>
-                    {documentList.length > 0 ? (
-                        <ul>
-                            {documentList.map((document) => {
-                                return (
-                                    <li key={document._id}>
-                                        <Link to={`/document/${document._id}`}>
-                                            {document.name}
-                                            {document._id}
-                                        </Link>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    ) : (
-                        <p>No documents found.</p>
-                    )}
-                </div>
-            </div>
+    fetchDocuments();
+  }, []);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const formType = document.getElementById("formType").value;
+    if (formType === "intake") {
+      navigate("/intake");
+    } else if (formType === "order") {
+      navigate("/order");
+    } else if (formType === "custom") {
+      const id = (
+        await putData({
+          title: "New",
+          client: "Untitled",
+          type: "custom",
+          author: "Untitled",
+        })
+      )["_id"];
+      navigate(`/document/${id}`);
+    }
+  };
+
+  return (
+    <div style={{ margin: "2vw 2vw 2vw 2vw" }}>
+      <h1>HOME</h1>
+      <hr />
+      <div className="row">
+        <div className="col-6">
+          <h4>Create New Form</h4>
+          <form onSubmit={handleFormSubmit}>
+            <select id="formType">
+              <option value="" disabled selected hidden>
+                ---
+              </option>
+              <option value="intake">Intake</option>
+              <option value="order">Order</option>
+              <option value="custom">Custom</option>
+            </select>
+            <input type="submit" value="create" style={{ marginLeft: "1vw" }} />
+          </form>
+          <Search />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
